6d715a45b4d45a0d550c8c861e659b796daae2d6
bastengao <bastengao@gmail.com>
2013-01-10 23:36:57 +0800

    添加常用 result 类型. issue #13

---
+++ struts2-freeroute-plugin/src/main/java/com/bastengao/struts2/freeroute/ChainResult.java
@@ -0,0 +1,10 @@
+package com.bastengao.struts2.freeroute;
+
+/**
+ * TODO
+ * 参考: http://struts.apache.org/2.3.8/docs/chain-result.html
+ * @author bastengao
+ * @date 13-1-10 23:32
+ */
+public class ChainResult  extends Result{
+}
---
+++ struts2-freeroute-plugin/src/main/java/com/bastengao/struts2/freeroute/HttpHeaderResult.java
@@ -0,0 +1,11 @@
+package com.bastengao.struts2.freeroute;
+
+/**
+ * TODO
+ * 参考: http://struts.apache.org/2.3.8/docs/httpheader-result.html
+ *
+ * @author bastengao
+ * @date 13-1-10 23:34
+ */
+public class HttpHeaderResult extends Result {
+}
