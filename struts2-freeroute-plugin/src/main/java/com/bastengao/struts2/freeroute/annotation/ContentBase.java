4cb4654aec16605817d03de42c389f4f0cbe5329
bastengao <bastengao@gmail.com>
2013-01-12 09:55:07 +0800

    完善注释

--- struts2-freeroute-plugin/src/main/java/com/bastengao/struts2/freeroute/annotation/ContentBase.java
+++ struts2-freeroute-plugin/src/main/java/com/bastengao/struts2/freeroute/annotation/ContentBase.java
@@ -6,13 +6,20 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 内容目录
+ * 内容路径
  *
  * @author bastengao
  * @date 12-12-16 15:24
+ * @since 1.0
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ContentBase {
+    /**
+     * 内容基路径
+     *
+     * @return
+     * @since 1.0
+     */
     String value() default "";
 }
