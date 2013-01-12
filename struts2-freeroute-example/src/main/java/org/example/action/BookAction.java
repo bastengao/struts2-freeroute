2144cf906d6e033135a45b8cd525d0b161b2bc6e
bastengao <bastengao@gmail.com>
2013-01-12 20:26:59 +0800

    多个 controller 后缀例子

---
+++ struts2-freeroute-example/src/main/java/org/example/action/BookAction.java
@@ -0,0 +1,15 @@
+package org.example.action;
+
+import com.bastengao.struts2.freeroute.Results;
+import com.bastengao.struts2.freeroute.annotation.Route;
+
+/**
+ * @author bastengao
+ * @date 13-1-12 20:21
+ */
+public class BookAction {
+    @Route("/books")
+    public String execute() {
+        return Results.html("/my-result");
+    }
+}
