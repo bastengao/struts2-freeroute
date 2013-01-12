f3a0d582d61cb6d11af03d46e7ee430b39f8842f
bastengao <bastengao@gmail.com>
2013-01-12 16:45:13 +0800

    完善 ResultTest

--- struts2-freeroute-plugin/src/test/java/com/bastengao/struts2/freeroute/ResultTest.java
+++ struts2-freeroute-plugin/src/test/java/com/bastengao/struts2/freeroute/ResultTest.java
@@ -13,26 +13,34 @@ public class ResultTest {
     @Test
     public void testResult() {
         String value = Result.create().param("location", "value.jsp").done();
-        System.out.println(value);
+        Assert.assertNotNull(value);
 
         value = Result.create("freemarker").location("value.jsp").done();
-        System.out.println(value);
+        Assert.assertNotNull(value);
 
         value = Result.create("chain").param("name1", "value1").param("name2", "value2").done();
-        System.out.println(value);
+        Assert.assertNotNull(value);
+    }
+
+    @Test
+    public void testResult2() {
+        String result = Result.create().location("/abc.html").done();
+        Assert.assertNotNull(result);
     }
 
     @Test
     public void testReadJson() throws JSONException {
         JSONObject jsonObject = new JSONObject("{\"name\":\"value\"}");
         Assert.assertNotNull(jsonObject);
+        Assert.assertEquals("value", jsonObject.getString("name"));
 
         JSONObject jsonObject2 = new JSONObject("{name:value, name2: value2}");
         Assert.assertNotNull(jsonObject2);
+        Assert.assertEquals("value", jsonObject2.getString("name"));
 
         JSONObject jsonObject3 = new JSONObject("{name:value, name2: \"${value2}\"}");
-        System.out.println(jsonObject3);
         Assert.assertNotNull(jsonObject3);
+        Assert.assertEquals("${value2}", jsonObject3.getString("name2"));
     }
 
     @Test(expected = JSONException.class)
