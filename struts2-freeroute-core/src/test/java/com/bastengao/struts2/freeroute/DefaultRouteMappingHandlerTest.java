016245d73bceccb733298319fcb01c15650e07d0
bastengao <bastengao@gmail.com>
2013-03-02 11:28:09 +0800

    test containsSameRoute

--- struts2-freeroute-core/src/test/java/com/bastengao/struts2/freeroute/DefaultRouteMappingHandlerTest.java
+++ struts2-freeroute-core/src/test/java/com/bastengao/struts2/freeroute/DefaultRouteMappingHandlerTest.java
@@ -1,7 +1,9 @@
 package com.bastengao.struts2.freeroute;
 
+import com.bastengao.struts2.freeroute.annotation.MethodType;
 import com.bastengao.struts2.freeroute.annotation.Route;
 import com.bastengao.struts2.freeroute.helper.RouteHelper;
+import com.example.action.BookController;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.junit.Assert;
@@ -34,16 +36,142 @@ public class DefaultRouteMappingHandlerTest {
     }
 
     @Test
-    public void testContainsSameRoute() {
-        Class clazz = this.getClass();
-        Method method = ReflectUtil.methodOf(clazz, "testContainsSameRoute");
+    public void testContainsSameRoutePath() {
+        Class clazz = BookController.class;
+        Method method = ReflectUtil.methodOf(clazz, "execute");
 
         Route route = RouteHelper.mockRoute("/path");
-
         RouteMapping routeMapping = new RouteMapping(route, clazz, method);
 
         Route route2 = RouteHelper.mockRoute("/path");
+        RouteMapping routeMapping2 = new RouteMapping(route2, clazz, method);
 
+
+        RouteMapping sameRoute = DefaultRouteMappingHandler.containsSameRoute(routeMapping2, Lists.newArrayList(routeMapping));
+        Assert.assertNotNull(sameRoute);
+    }
+
+    @Test
+    public void testContainsSameRoutePath2() {
+        Class clazz = BookController.class;
+        Method method = ReflectUtil.methodOf(clazz, "execute");
+
+        Route route = RouteHelper.mockRoute("/path");
+        RouteMapping routeMapping = new RouteMapping(route, clazz, method);
+
+        Route route2 = RouteHelper.mockRoute("/path2");
+        RouteMapping routeMapping2 = new RouteMapping(route2, clazz, method);
+
+
+        RouteMapping sameRoute = DefaultRouteMappingHandler.containsSameRoute(routeMapping2, Lists.newArrayList(routeMapping));
+        Assert.assertNull(sameRoute);
+    }
+
+    @Test
+    public void testContainsSameRouteHttpMethod() {
+        Class clazz = BookController.class;
+        Method method = ReflectUtil.methodOf(clazz, "execute");
+
+        Route route = RouteHelper.mockRoute("/path", new MethodType[]{MethodType.GET}, new String[]{});
+        RouteMapping routeMapping = new RouteMapping(route, clazz, method);
+
+        Route route2 = RouteHelper.mockRoute("/path", new MethodType[]{MethodType.GET}, new String[]{});
+        RouteMapping routeMapping2 = new RouteMapping(route2, clazz, method);
+
+
+        RouteMapping sameRoute = DefaultRouteMappingHandler.containsSameRoute(routeMapping2, Lists.newArrayList(routeMapping));
+        Assert.assertNotNull(sameRoute);
+    }
+
+    @Test
+    public void testContainsSameRouteHttpMethod2() {
+        Class clazz = BookController.class;
+        Method method = ReflectUtil.methodOf(clazz, "execute");
+
+        Route route = RouteHelper.mockRoute("/path", new MethodType[]{MethodType.GET}, new String[]{});
+        RouteMapping routeMapping = new RouteMapping(route, clazz, method);
+
+        Route route2 = RouteHelper.mockRoute("/path", new MethodType[]{MethodType.POST}, new String[]{});
+        RouteMapping routeMapping2 = new RouteMapping(route2, clazz, method);
+
+
+        RouteMapping sameRoute = DefaultRouteMappingHandler.containsSameRoute(routeMapping2, Lists.newArrayList(routeMapping));
+        Assert.assertNull(sameRoute);
+    }
+
+    @Test
+    public void testContainsSameRouteHttpMethod3() {
+        Class clazz = BookController.class;
+        Method method = ReflectUtil.methodOf(clazz, "execute");
+
+        Route route = RouteHelper.mockRoute("/path", new MethodType[]{MethodType.GET}, new String[]{});
+        RouteMapping routeMapping = new RouteMapping(route, clazz, method);
+
+        Route route2 = RouteHelper.mockRoute("/path", new MethodType[]{MethodType.POST, MethodType.GET}, new String[]{});
+        RouteMapping routeMapping2 = new RouteMapping(route2, clazz, method);
+
+
+        RouteMapping sameRoute = DefaultRouteMappingHandler.containsSameRoute(routeMapping2, Lists.newArrayList(routeMapping));
+        Assert.assertNotNull(sameRoute);
+    }
+
+    @Test
+    public void testContainsSameRouteHttpParam() {
+        Class clazz = BookController.class;
+        Method method = ReflectUtil.methodOf(clazz, "execute");
+
+        Route route = RouteHelper.mockRoute("/path", new MethodType[]{}, new String[]{"param"});
+        RouteMapping routeMapping = new RouteMapping(route, clazz, method);
+
+        Route route2 = RouteHelper.mockRoute("/path", new MethodType[]{}, new String[]{"param"});
+        RouteMapping routeMapping2 = new RouteMapping(route2, clazz, method);
+
+
+        RouteMapping sameRoute = DefaultRouteMappingHandler.containsSameRoute(routeMapping2, Lists.newArrayList(routeMapping));
+        Assert.assertNotNull(sameRoute);
+    }
+
+    @Test
+    public void testContainsSameRouteHttpParam2() {
+        Class clazz = BookController.class;
+        Method method = ReflectUtil.methodOf(clazz, "execute");
+
+        Route route = RouteHelper.mockRoute("/path", new MethodType[]{}, new String[]{"param"});
+        RouteMapping routeMapping = new RouteMapping(route, clazz, method);
+
+        Route route2 = RouteHelper.mockRoute("/path", new MethodType[]{}, new String[]{"param2"});
+        RouteMapping routeMapping2 = new RouteMapping(route2, clazz, method);
+
+
+        RouteMapping sameRoute = DefaultRouteMappingHandler.containsSameRoute(routeMapping2, Lists.newArrayList(routeMapping));
+        Assert.assertNull(sameRoute);
+    }
+
+    @Test
+    public void testContainsSameRouteHttpParam3() {
+        Class clazz = BookController.class;
+        Method method = ReflectUtil.methodOf(clazz, "execute");
+
+        Route route = RouteHelper.mockRoute("/path", new MethodType[]{}, new String[]{"param2!=value", "param"});
+        RouteMapping routeMapping = new RouteMapping(route, clazz, method);
+
+        Route route2 = RouteHelper.mockRoute("/path", new MethodType[]{}, new String[]{"param", "param2!=value"});
+        RouteMapping routeMapping2 = new RouteMapping(route2, clazz, method);
+
+
+        RouteMapping sameRoute = DefaultRouteMappingHandler.containsSameRoute(routeMapping2, Lists.newArrayList(routeMapping));
+        Assert.assertNotNull(sameRoute);
+    }
+
+    @Test
+    public void testContainsSameRoute() {
+        Class clazz = BookController.class;
+        Method method = ReflectUtil.methodOf(clazz, "execute");
+
+        Route route = RouteHelper.mockRoute("/path", new MethodType[]{MethodType.GET}, new String[]{"param2!=value", "param"});
+        RouteMapping routeMapping = new RouteMapping(route, clazz, method);
+
+        Route route2 = RouteHelper.mockRoute("/path", new MethodType[]{MethodType.POST, MethodType.GET}, new String[]{"param", "param2!=value"});
         RouteMapping routeMapping2 = new RouteMapping(route2, clazz, method);
 
 
